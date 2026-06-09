21bfef2b39a0add85df7f1aeccbce368e0a1ca52
CaroReolon <carolina.reolon@e14.io>
2026-06-08 22:53:53 -0300

    Fix to have a domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 allowed_origins = [
   "http://localhost:5173",
   "http://18.220.250.174",
+  "http://pencaworldcup.webhop.me",
+  "https://pencaworldcup.webhop.me",
   ENV["FRONTEND_URL"]
 ].compact.uniq
 
