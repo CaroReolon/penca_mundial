0483477dd904e979414e2da5238c1da3b64841c4
CaroReolon <carolina.reolon@e14.io>
2026-06-26 00:12:10 -0300

    add papertrail object

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_06_26_024641) do
+ActiveRecord::Schema[7.1].define(version: 2026_06_26_030000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -223,6 +223,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_06_26_024641) do
     t.string "item_type", null: false
     t.string "event", null: false
     t.text "object"
+    t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
