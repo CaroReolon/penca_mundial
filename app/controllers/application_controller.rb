0e56fbe38f61a1a8eef4921292f8cdf3c041dae8
CaroReolon <carolina.reolon@e14.io>
2026-06-26 00:19:19 -0300

    fix papertrail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::API
-  include PaperTrail::Rails::Controller
-
   before_action :set_paper_trail_whodunnit
 
-  def user_for_paper_trail
-    if current_user
+  def set_paper_trail_whodunnit
+    PaperTrail.request.whodunnit = if current_user
       "user:#{current_user.id} (#{current_user.email})"
     else
       "app"
