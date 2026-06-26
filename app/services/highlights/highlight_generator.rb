11e9f93035df12ccb3a733b063b09ba2c67cbb3e
CaroReolon <carolina.reolon@e14.io>
2026-06-26 01:55:35 -0300

    Add error handling in highlights

--- app/services/highlights/highlight_generator.rb
+++ app/services/highlights/highlight_generator.rb
@@ -15,9 +15,9 @@ module Highlights
 
     def self.call(play_group:)
       CALCULATORS.each do |calculator|
-        calculator.call(
-          play_group: play_group
-        )
+        calculator.call(play_group: play_group)
+      rescue => e
+        Rails.logger.error "[HighlightGenerator] #{calculator} failed: #{e.class} #{e.message}\n#{e.backtrace.first(5).join("\n")}"
       end
     end
   end
